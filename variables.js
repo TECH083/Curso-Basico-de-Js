733e6ed7d81146b8049e7570a00458e43ce4f747
Cristian Munoz <munozcristian226@gmail.com>
2021-02-16 13:57:29 -0500

    Se agrega el codigo Variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+
+var nombre = "Oscar"
+
+// declarar
+
+var edad;
+
+// inicializar
+edad = 30;
+
+var elementos = ["Computadora" , "Celular"];
+
+var persona = {
+    nombre: "Cristian",
+    edad: 30
+}
+
