bf6c7b799e64f7460fda0bbe84da1ea84c45e1c0
Cristian Munoz <munozcristian226@gmail.com>
2021-02-16 14:19:41 -0500

    Se agrega el codigo Funciones.js

---
+++ funciones.js
@@ -0,0 +1,18 @@
+// Declarativas
+function miFuncion(){
+    return 3;
+}
+
+
+//Expresión
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+
+//Funcion Declarativa
+function sumar (a,b){
+    var resultado = a + b;
+    return resultado;
+}
